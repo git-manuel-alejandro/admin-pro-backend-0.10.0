9458f55a41618cabffe755a35571d5d915bcddf8
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-05-10 15:11:44 -0400

    upload files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,10 +1,11 @@
 const { response } = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const fileUpload = (req, res = response) => {
   const tipo = req.params.tipo;
   const id = req.params.id;
 
-  const tiposValidos = ["hospitales", "medicos", "usuarios"];
+  const tiposValidos = ["hospital", "medicos", "usuarios"];
 
   if (!tiposValidos.includes(tipo)) {
     return res.status(400).json({
@@ -20,9 +21,42 @@ const fileUpload = (req, res = response) => {
     });
   }
 
-  res.json({
-    ok: true,
-    msg: "fileUpload",
+  // Procesar imagen
+  const file = req.files.imagen;
+  const nombreCortado = file.name.split(".");
+  const extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+  //validar extension
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+  if (!extensionesValidas.includes(extensionArchivo)) {
+    return res.status(400).json({
+      ok: false,
+      msg: "Not valid format files",
+    });
+  }
+
+  //Generar nombre archivo
+  const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
+
+  //Path guardar archivo
+  const path = `./uploads/${tipo}/${nombreArchivo}`;
+  // const path = __dirname + `/uploads/${tipo}/${nombreArchivo}`;
+
+  // Use the mv() method to place the file somewhere on your server
+
+  file.mv(path, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        ok: true,
+        msg: "Error al mover la imagen",
+      });
+    }
+    res.json({
+      ok: true,
+      msg: "fileUpload",
+      nombre: nombreArchivo,
+    });
   });
 };
 
